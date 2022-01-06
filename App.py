db26dcc36e4c09b9a6e19722dbe60e13661c3f59
Chris Rodriguez <Crodriguez4443@gmail.com>
2022-01-06 14:02:12 -0500

    Finished cleaning data: 1. Create a copy of constants and Clean data     a. height converted to integer     b. experienced -> Boolean(True or False)     c. Guardians -> list & delete " and " 2. Create balanced teams function (3)     a. 3 teams with equal number of experienced & inexperienced players (6 players on each team)

--- App.py
+++ App.py
@@ -2,13 +2,13 @@
 Statistics for a kids basketball team.
 Goal:
 1. Create a copy of constants and Clean data
-    a. height converted to integer
-    b. experienced -> Boolean(True or False)
-    c. Guardians -> list & delete " and "
+    a. height converted to integer ********************
+    b. experienced -> Boolean(True or False)  ********************
+    c. Guardians -> list & delete " and "  ********************
 2. Create balanced teams function (3)
-    a. 3 teams with equal number of experienced & inexperienced players
+    a. 3 teams with equal number of experienced & inexperienced players (6 players on each team) ********************
 3. Display stats of teams
-    a.display team name
+    a. display team name
     b. Total players on that team as an integer
     c. The player names as strings separated by commas
     d. number of inexperienced players on that team
@@ -26,25 +26,75 @@ Extra credit
 from constants import TEAMS
 from constants import PLAYERS
 import copy
-import re
+
+Panthers = []
+Bandits = []
+Warriors = []
+
+# Copy PLAYERS to a new list to be mutated
+all_players = copy.deepcopy(PLAYERS)
+
 
 def clean_up():
-    all_players = copy.copy(PLAYERS)
-    for player in all_players:
-        for key, value in player.items():
-            # CONVERT HEIGHT from string to int
-            if key == "height":
-                re.sub("[^0-9]", "", value)
-                print(value)
 
-        # comma delineate guardian names
+    # Data cleaning (height, experience, guardians)
+    # sort players in all_players by experience
+    from operator import itemgetter
+    sorted(all_players, key=(itemgetter("experience")))
+    for i in range(len(all_players)):
+        height_in = all_players[i]["height"][0:2]
+        all_players[i]["height"] = height_in
+        # print(all_players[i]["height"])
 
-        for key, value in player.items():
-            if key == "guardians":
-                value.split(" and ")
-                print(value)
+        if all_players[i]["experience"] == "YES":
+            all_players[i]["experience"] = bool("TRUE")
+        else:
+            all_players[i]["experience"] = bool("FALSE")
+        # print(all_players[i]["experience"])
+        # print(type(all_players[i]["experience"]))
+
+        guardian_names = all_players[i]["guardians"].split(" and ")
+        all_players[i]["guardians"] = guardian_names
+        # print(all_players[i]["guardians"])
+    return all_players
 
 
+def draft_players():
+    # copy code over and figure out how to make it work
+    # print(all_players)
+    all_teams = [Panthers, Bandits, Warriors]
+    teams_len = len(all_teams)
+    print(teams_len)
+    for player in range(len(all_players)):
+        all_teams[player % teams_len].append(all_players[player])
+    print("Panthers: \n", Panthers, "\n\n", "Bandits: \n", Bandits, "\n\n", Warriors)
+    return Panthers, Bandits, Warriors
+
+
+""" 
+    print(all_players)
+    experienced_players = set()
+    un_experienced_players = set()
+    while len(experienced_players) != 3:
+        for i in all_players:
+            if i["experience"] == "TRUE":
+                experienced_players.add(i)
+                team_name.extend(experienced_players)
+                print(experienced_players)
+                return experienced_players
+
+        print(experienced_players)
+    while len(un_experienced_players) != 3:
+        for i in all_players:
+            if i["experience"] == "FALSE":
+                un_experienced_players.add(i)
+                team_name.append(un_experienced_players)
+                return un_experienced_players
+
+    return team_name
+"""
 
 if __name__ == "__main__":
     clean_up()
+
+draft_players()
