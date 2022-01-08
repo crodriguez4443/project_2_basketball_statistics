1a53a649f75f5da9fe12b981c3b268d6b44f3dd9
Chris Rodriguez <Crodriguez4443@gmail.com>
2022-01-08 10:00:56 -0500

    interface started
    debugging data cleaning. Come back point if it ends up looking like a mess

--- App.py
+++ App.py
@@ -8,8 +8,8 @@ Goal:
 2. Create balanced teams function (3)
     a. 3 teams with equal number of experienced & inexperienced players (6 players on each team) ********************
 3. Display stats of teams
-    a. display team name
-    b. Total players on that team as an integer
+    a. display team name *
+    b. Total players on that team as an integer *
     c. The player names as strings separated by commas
     d. number of inexperienced players on that team
     e. number of experienced players on that team
@@ -26,10 +26,14 @@ Extra credit
 from constants import TEAMS
 from constants import PLAYERS
 import copy
+from time import sleep
+
 
 Panthers = []
 Bandits = []
 Warriors = []
+team_names_str = ["Panthers", "Bandits", "Warriors"]
+all_teams = [Panthers, Bandits, Warriors]
 
 # Copy PLAYERS to a new list to be mutated
 all_players = copy.deepcopy(PLAYERS)
@@ -40,9 +44,10 @@ def clean_up():
     # Data cleaning (height, experience, guardians)
     # sort players in all_players by experience
     from operator import itemgetter
-    sorted(all_players, key=(itemgetter("experience")))
+    asp = sorted(all_players, key=(itemgetter("experience")))
     for i in range(len(all_players)):
         height_in = all_players[i]["height"][0:2]
+        int(height_in)
         all_players[i]["height"] = height_in
         # print(all_players[i]["height"])
 
@@ -62,39 +67,89 @@ def clean_up():
 def draft_players():
     # copy code over and figure out how to make it work
     # print(all_players)
-    all_teams = [Panthers, Bandits, Warriors]
     teams_len = len(all_teams)
-    print(teams_len)
     for player in range(len(all_players)):
         all_teams[player % teams_len].append(all_players[player])
-    print("Panthers: \n", Panthers, "\n\n", "Bandits: \n", Bandits, "\n\n", Warriors)
+    # print("Panthers: \n", Panthers, "\n\n", "Bandits: \n", Bandits, "\n\n", Warriors)
     return Panthers, Bandits, Warriors
 
 
-""" 
-    print(all_players)
-    experienced_players = set()
-    un_experienced_players = set()
-    while len(experienced_players) != 3:
-        for i in all_players:
-            if i["experience"] == "TRUE":
-                experienced_players.add(i)
-                team_name.extend(experienced_players)
-                print(experienced_players)
-                return experienced_players
+def team_stats(team_name, selection):
+    # give name of team
+    print(f"\nTeam Name: {team_names_str[selection]}")
+    # number of members on the team
+    number_of_players = len(all_teams[selection])
+    print(f"Number of Players: {number_of_players}")
+    # The player names as strings separated by commas
+    team_members = []
+    for count, i in enumerate(team_name):
+        team_members.append(team_name[count]["name"])
+    team_members_string = ", ".join(team_members)
+    print(f"Players: {team_members_string}")
 
-        print(experienced_players)
-    while len(un_experienced_players) != 3:
-        for i in all_players:
-            if i["experience"] == "FALSE":
-                un_experienced_players.add(i)
-                team_name.append(un_experienced_players)
-                return un_experienced_players
+    # number of inexperienced players on that team
+    ip = 0
+    ep = 0
+    for i in team_name:
+        if i["experience"] is False:
+            ip += 1
+        elif i["experience"] is True:
+            ep += 1
+    print(f"Inexperienced Players: {ip}")
+    # number of experienced players on that team
+    print(f"Experienced Players: {ep}")
+    # the average height of the team
+    sum_heights = 0
+    for i in team_name:
+        sum_heights = sum_heights + i["height"]
+    avg_height = sum_heights / len(team_name)
+    print(f"Average player height: {avg_height} inches")
+    # the guardians of all the players on that team (as a comma-separated string)
 
-    return team_name
-"""
+    # The formatting you use to display is up to you.
 
-if __name__ == "__main__":
-    clean_up()
+# User interface
+def statistics_finder():
+    print("***BASKETBALL STATS***")
+    try:
+        start_select = str(input("Select Option \n    a) View teams \n    b) Quit \n"))
+        if start_select.lower() == "a":
+            print("\n\nSELECT A TEAM TO VIEW")
+            print("    a) Panthers \n    b) Bandits \n    c) Warriors")
+            try:
+                mm_select = input("Choose one team")
+                mm_select.lower()
+                if mm_select == "a":
+                    print("Accessing Panthers team view...")
+                    sleep(.25)
+                    """
+                    loading = "##########"
+                    for letter in loading:
+                        print(letter)
+                        sleep(.1) """
+                    team_stats(Panthers, 0)
+                elif mm_select == "b":
+                    print("Bandits team viewer")
+                elif mm_select == "c":
+                    print("Warriors team viewer")
+                else:
+                    raise ValueError
+            except ValueError:
+                print("Please provide a valid response")
+            # Bring to main menu
+        elif start_select.lower() == "b":
+            print("Goodbye")
+        else:
+            raise ValueError
+    except ValueError:
+        print("Please provide a valid response")
+
+# if __name__ == "__main__":
+
+
+clean_up()
 
 draft_players()
+
+# statistics_finder()
+
